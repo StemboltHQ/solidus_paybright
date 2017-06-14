14f37b4c13794047b800ae2dfdb305f1ae53589a
Alessandro Lepore <lepore.ale@gmail.com>
2017-06-14 14:32:12 +0200

    Add payment redirect support to CheckoutController
    `CheckoutController` never had the concept of redirecting the user to the
    "offsite" payment method site.
    This situation was handled in various bad ways in the past, mostly with the
    substitution of the checkout submit button with a link tag, using some JS.
    
    This commit adds an override to CheckoutController to check if the active
    payment wants to redirect to its site, before proceeding to the next checkout
    step.
    Different versions of `#update` overrides are loaded for different Solidus
    versions.
    
    The `Payment` class gets a new method called `#redirect_url`.
    In this basic implementation it just delegates to the payment method, but this
    interface can be used as a base for different setups. For example, some
    payment methods may need to use, or even persist, a dynamically generated
    redirect URL.

---
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -0,0 +1,75 @@
+module SolidusPaybright
+  module CheckoutControllerDecorator
+    def update
+      if SolidusSupport.solidus_gem_version >= Gem::Version.new("2.2.0")
+        update_v2_2
+      else
+        update_v1_2
+      end
+    end
+
+    private
+
+    def update_v2_2
+      if update_order
+
+        assign_temp_address
+        return if follow_payment_redirect
+
+        unless transition_forward
+          redirect_on_failure
+          return
+        end
+
+        if @order.completed?
+          finalize_order
+        else
+          send_to_next_state
+        end
+
+      else
+        render :edit
+      end
+    end
+
+    def update_v1_2
+      if Spree::OrderUpdateAttributes.new(@order, update_params, request_env: request.headers.env).apply
+        @order.temporary_address = !params[:save_user_address]
+        return if follow_payment_redirect
+
+        success = if @order.state == 'confirm'
+          @order.complete
+        else
+          @order.next
+        end
+        if !success
+          flash[:error] = @order.errors.full_messages.join("\n")
+          redirect_to(checkout_state_path(@order.state)) && return
+        end
+
+        if @order.completed?
+          @current_order = nil
+          flash.notice = Spree.t(:order_processed_successfully)
+          flash['order_completed'] = true
+          redirect_to completion_route
+        else
+          redirect_to checkout_state_path(@order.state)
+        end
+      else
+        render :edit
+      end
+    end
+
+    def follow_payment_redirect
+      return unless params[:state] == "payment"
+
+      payment = @order.payments.valid.last
+      if redirect_url = payment.try(:redirect_url)
+        redirect_to redirect_url
+        true
+      end
+    end
+  end
+end
+
+Spree::CheckoutController.prepend SolidusPaybright::CheckoutControllerDecorator
---
+++ spec/controllers/spree/checkout_controller_spec.rb
@@ -0,0 +1,50 @@
+require "spec_helper"
+
+describe Spree::CheckoutController, type: :controller do
+  let(:token) { 'some_token' }
+  let(:user) { create(:user) }
+  let(:order) { create(:order_with_line_items) }
+  let(:payment_method) { create(:check_payment_method) }
+
+  before do
+    allow(controller).to receive_messages try_spree_current_user: user
+    allow(controller).to receive_messages spree_current_user: user
+    allow(controller).to receive_messages current_order: order
+    allow(order).to receive_messages checkout_steps: ["address", "delivery", "payment", "confirm", "complete"]
+    order.update_attributes! user: user
+  end
+
+  def patch_payment
+    patch :update, params: {
+      state: "payment",
+      order: {
+        payments_attributes: [payment_method_id: payment_method.id]
+      }
+    }
+  end
+
+  context "PATCH #update" do
+    context "Using a payment method without a redirect url" do
+      it "proceed to the confirm step" do
+        patch_payment
+        expect(response).to redirect_to(spree.checkout_state_path(:confirm))
+      end
+    end
+
+    context "Using a payment method with a redirect url" do
+      before do
+        allow_any_instance_of(
+          Spree::PaymentMethod
+        ).to receive_messages(
+          has_redirect_url?: true,
+          redirect_url: "http://example.com"
+        )
+      end
+
+      it "redirects to the payment method site" do
+        patch_payment
+        expect(response).to redirect_to("http://example.com")
+      end
+    end
+  end
+end
