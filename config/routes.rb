56f46c43bfbb2ef50739b2f5dd2427386cd2c832
Alessandro Lepore <lepore.ale@gmail.com>
2017-06-19 15:56:53 +0200

    Add the required routes
    The Paybright integration requires 3 routes for the communication of the
    payment result and for the redirects in case of success or failure.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Spree::Core::Engine.routes.draw do
-  # Add your extension routes here
+  post "/paybright/callback", to: "paybright#callback", as: :paybright_callback
+  get "/paybright/cancel/:payment_id", to: "paybright#cancel", as: :paybright_cancel
+  get "/paybright/complete", to: "paybright#complete", as: :paybright_complete
 end
