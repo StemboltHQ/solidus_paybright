1a3ce4279dae4111bbee9bb1b4feef2017f890e1
Alessandro Lepore <lepore.ale@gmail.com>
2017-06-20 09:02:42 +0200

    Add API endpoints to the Configuration object

--- app/models/solidus_paybright/configuration.rb
+++ app/models/solidus_paybright/configuration.rb
@@ -9,5 +9,17 @@ module SolidusPaybright
     def live_redirect_url
       @live_redirect_url ||= "" # TODO
     end
+
+    attr_writer :test_api_endpoint
+    def test_api_endpoint
+      # no trailing slash
+      @test_api_endpoint ||= "https://devapi.paybright.com/api"
+    end
+
+    attr_writer :live_api_endpoint
+    def live_api_endpoint
+      # no trailing slash
+      @live_api_endpoint ||= "" # TODO
+    end
   end
 end
